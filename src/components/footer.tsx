ad7d38f564bbcf1947d8b1766e28a36f5ab80dc6
Abhishek Sunil <abhisheksunil92@gmail.com>
2024-01-31 14:14:41 +0530

    add wallet ui

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <div className="w-full px-6 border-t border-t-gray-700 py-2 text-center">
+      One Wallet to Rule Them All. Built with ❤️
+    </div>
+  );
+}
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -6,26 +6,23 @@ import {
   RainbowKitProvider,
 } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
-import { createConfig, WagmiProvider } from "wagmi";
+import { createConfig, WagmiProvider, http } from "wagmi";
 import { sepolia } from "wagmi/chains";
-import { publicProvider } from "wagmi/providers/public";
-
-const config = getDefaultWallets({
-  appName: "one-wallet",
-  projectId: `${process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID}`,
-  chains: [sepolia],
-});
+import { injected } from "wagmi/connectors";
+import { createClient } from "viem";
 
 const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
+  chains: [sepolia],
+  connectors: [injected()],
+  client({ chain }) {
+    return createClient({ chain, transport: http() });
+  },
 });
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={wagmiConfig}>
-      <RainbowKitProvider chains={chains} theme={darkTheme()}>
+      <RainbowKitProvider chains={[sepolia]} theme={darkTheme()}>
         {children}
       </RainbowKitProvider>
     </WagmiProvider>
